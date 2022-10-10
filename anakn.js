b03a5bfa39a0cf944eff489448689dba9759bbbb
itsankan03a <ankanbarman3878@gmail.com>
2022-10-10 00:34:34 -0700

    ankan

---
+++ anakn.js
@@ -0,0 +1,9 @@
+function nameshow(){
+    //var x=document.getElementById('name').value;
+    //document.getElementById('valueimport').innerHTML ='hello'+ x;
+    //var x='10';
+    //var y='10';
+    //console.log(Number(x)+Number(y))
+    var x=document.getElementById('amount').tabIndex
+    document.write(x)
+}
